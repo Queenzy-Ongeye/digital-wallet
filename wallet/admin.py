9e906bb33fd915485be1f089e4171c8e03b8e566
Queen-02 <queenzyblack176@gmail.com>
2022-07-29 08:30:55 +0300

    first commit

--- wallet/admin.py
+++ wallet/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import Customer
+from .models import Wallet
 # Register your models here.
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email")
+    search_fields = ("first_name", "last_name")
+
+admin.site.register(Customer, CustomerAdmin)
+
+
+class WalletAdmin(admin.ModelAdmin):
+    list_display = ("customer", "balance", "date")
+    search_fields = ("customer","balance")
+admin.site.register(Wallet, WalletAdmin)
+
--- wallet/models.py
+++ wallet/models.py
@@ -11,8 +11,23 @@ class Customer(models.Model):
     id_number = models.CharField(max_length=10)
     phone_number = models.CharField(max_length=15)
     email = models.EmailField()
+    # profile = models.ImageField()
+    # signature = models.ImageField()
+    employment_status = models.BooleanField(default=False)
+    # marital_status = models.CharField(max_length=20)
 
 
 class Wallet(models.Model):
-    userId = models.CharField(max_length=5)
-    userName = models.CharField(max_length=50)
+   customer = models.OneToOneField('Customer',on_delete=models.DO_NOTHING)
+   date = models.DateTimeField(auto_now_add=True)
+   pin = models.IntegerField()
+   isActive = models.BooleanField(default=False)
+   balance = models.IntegerField()
+
+
+class Account(models.Model):
+    accType = models.CharField(max_length=255, blank=True)
+    accName = models.CharField(max_length=255, blank=True)
+    savings = models.IntegerField()
+    wallet = models.ForeignKey('Wallet', blank=True, null=True, default_auto_field=True)
+    destination = models.CharField(max_length=255, blank=True)
