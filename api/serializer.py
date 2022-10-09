d0a385428322a14bd71573727eccce37578aaef1
Queen-02 <queenzyblack176@gmail.com>
2022-10-09 15:53:55 +0300

    created model routers

--- api/serializer.py
+++ api/serializer.py
@@ -1,7 +1,37 @@
+from dataclasses import fields
 from rest_framework import serializers
-from wallet.models import Customer
+from wallet.models import Customer, Wallet
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = ('first_name', 'last_name', 'email')
+
+class WalletSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Wallet
+        fields = ('customer', 'date', 'is_active', 'balance')
+
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('acc_type', 'acc_name', 'savings', 'wallet')
+
+class CardSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('card_type', 'card_name', 'card_account', 'pin_number', 'security_code')
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('transaction_type', 'transaction_code', 'customer', 'charge', 'origin_account', 'destination_account')
+
+class LoanSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('loan_type', 'loan_balance', 'duration', 'interest_rate', 'loan_status', 'loan_term')
+
+class ReceiptSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('receipt_date','total_amount', 'transaction')
+
+class NotificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('message', 'date_time', 'recipient')
--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path,include
 from rest_framework import routers
-from .views import CustomerViewSet
+from .views import CardViewSet, CustomerViewSet, LoanViewSet, NotificationViewSet, ReceiptViewSet, TransactionViewSet, WalletViewSet
 
 router = routers.DefaultRouter()
+
 router.register(r"customers", CustomerViewSet)
+router.create_wallet(r"wallets", WalletViewSet)
+router.create_card(r"cards", CardViewSet)
+router.create_transaction(r"transactions", TransactionViewSet)
+router.create_loan(r"loans", LoanViewSet)
+router.create_receipt(r"receipts", ReceiptViewSet)
+router.create_notification(r"notifications", NotificationViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
